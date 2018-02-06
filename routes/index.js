7f3cf9e4ef24e5ead26823ac842578620b62442a
H-sk sajir <sajir@MacBook-Pro-de-khenissi.local>
2018-02-06 12:15:42 +0100

    changement template

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
-
 var express = require('express');
 var router = express.Router();
 
-
-// router.post('/', function(req, res, next) {
-//  res.render('signup', { title: 'Express' });
-// });
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/signup', function(req, res, next) {
- res.render('index', { title: 'Express' });
-});
 module.exports = router;
