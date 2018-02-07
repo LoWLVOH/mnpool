19516d90fe3753c97ca40259c0fe2e95a5d96b87
Thomas Choquet <tchoquet74@gmail.com>
2018-02-07 12:33:01 +0100

    frontIndex

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var options = { server: { socketOptions: {connectTimeoutMS: 5000 } }};
+mongoose.connect('mongodb://mnp:azerty22@ds225308.mlab.com:25308/masternodepooldatabase',
+    options,
+    function(err) {
+     console.log(err);
+    }
+);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/postAds', function(req, res, next) {
+  res.render('postAds', { title: 'Express' });
+});
+
 
 module.exports = router;
